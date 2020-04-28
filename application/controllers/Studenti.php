7d97091a2bdeba6639a72a0e075d4059752f1d34
marti <mm@ukf.sk>
2020-04-28 16:17:02 +0200

    vytvorenie tabulky studenti

---
+++ application/controllers/Studenti.php
@@ -0,0 +1,19 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Studenti extends CI_Controller
+{
+	function __construct()
+	{
+		parent::__construct();
+		$this->load->helper('form');
+		$this->load->library('form_validation');
+		$this->load->model('Studenti_model');
+	}
+	public function index(){
+		$data = array();
+		$data['studenti'] = $this->Studenti_model->ZobrazStudentov();
+		$data['nazov'] = 'Zoznam študentov';
+		//nahratie zoznamu studentov
+		$this->load->view('studenti/index', $data);
+	}
+}
